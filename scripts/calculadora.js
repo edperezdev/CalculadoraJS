d3abf7b4f5279f96a59f48653aa64f0ffc7c2433
Edwin <edch90@gmail.com>
2020-02-26 20:36:55 -0500

    Se redujo líneas de código en el javascript

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,95 +1,37 @@
-// Variables
-
-let resultado = document.getElementById("resultado");
-let reset = document.getElementById("reset");
-let siete = document.getElementById("siete");
-let ocho = document.getElementById("ocho");
-let nueve = document.getElementById("nueve");
-let cuatro = document.getElementById("cuatro");
-let cinco = document.getElementById("cinco");
-let seis = document.getElementById("seis");
-let uno = document.getElementById("uno");
-let dos = document.getElementById("dos");
-let tres = document.getElementById("tres");
-let cero = document.getElementById("cero");
-let suma = document.getElementById("suma");
-let resta = document.getElementById("resta");
-let multiplicacion = document.getElementById("multiplicacion");
-let division = document.getElementById("division");
+const resultado = document.getElementById("resultado");
 let igual = document.getElementById("igual");
-let punto = document.getElementById("punto");
-let potencia = document.getElementById("potencia");
+let reset = document.getElementById("reset");
 
-// Clic en botones
+// Función para mostrar los números y signos de operación al hacer clic
 
-uno.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + uno.innerHTML;
-});
-dos.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + dos.innerHTML;
-});
-tres.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + tres.innerHTML;
-});
-cuatro.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + cuatro.innerHTML;
-});
-cinco.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + cinco.innerHTML;
-});
-seis.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + seis.innerHTML;
-});
-siete.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + siete.innerHTML;
-});
-ocho.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + ocho.innerHTML;
-});
-nueve.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + nueve.innerHTML;
-});
-cero.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + cero.innerHTML;
-});
+function clicenboton(element) {
+  this.element = document.getElementById(element);
+  this.element.onclick = () => {
+    event.preventDefault();
+    this.element.innerHTML !== "exp" ?
+    resultado.innerHTML = resultado.innerHTML + this.element.innerHTML
+    : resultado.innerHTML = resultado.innerHTML + `^`
+  };
+}
 
-punto.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + punto.innerHTML;
-});
+// Botones
 
-// Botones de operación
-suma.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + suma.innerHTML;
-});
-resta.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + resta.innerHTML;
-});
-multiplicacion.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + multiplicacion.innerHTML;
-});
-division.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + division.innerHTML;
-});
-
-potencia.addEventListener("click", function() {
-  event.preventDefault();
-  resultado.innerHTML = resultado.innerHTML + "^";
-});
+let siete = new clicenboton("siete");
+let ocho = new clicenboton("ocho");
+let nueve = new clicenboton("nueve");
+let cuatro = new clicenboton("cuatro");
+let cinco = new clicenboton("cinco");
+let seis = new clicenboton("seis");
+let uno = new clicenboton("uno");
+let dos = new clicenboton("dos");
+let tres =new clicenboton("tres");
+let cero =new clicenboton("cero");
+let suma =new clicenboton("suma");
+let resta = new clicenboton("resta");
+let multiplicacion = new clicenboton("multiplicacion");
+let division = new clicenboton("division");
+let punto = new clicenboton("punto");
+let potencia = new clicenboton("potencia");
 
 // Botón reset y back
 
